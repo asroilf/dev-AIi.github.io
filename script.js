8b9908609bf52dfdbf5ff9b9d62ea86d98497cbe
Asliddin Isroilov <57828415+dev-AIi@users.noreply.github.com>
2023-04-15 15:46:44 +0400

    problem with text fixed

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var slideIndex = 0;
+let slideIndex = 0;
 carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("hoc_pic");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+function slide() {
+  let i;
+  let a = document.getElementsByClassName("hoc_pic");
+  for (i = 0; i < a.length; i++) {
+    a[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  if (slideIndex > a.length) {slideIndex = 1}
+  a[slideIndex-1].style.display = "block";
+  setTimeout(slide, 2000); // Change image every 2 seconds
 }
